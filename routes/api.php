a57b0c177eb88f2b9a16c4dda3a6929234f7d8ff
Afeez Aziz <afeezaziz@gmail.com>
2023-03-29 13:11:52 +0800

    Update api.php
    Update the route with the controller functions.

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/login', [ApiController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {  
+
+    Route::get('/projects', [ApiController::class, 'list_projects']);
+    Route::post('/projects/search', [ApiController::class, 'search_project']);    
+    Route::get('/projects/{project_id}', [ApiController::class, 'get_project']);    
+    Route::post('/reports', [ApiController::class, 'update_report']);  
+    Route::get('/notifications', [ApiController::class, 'list_notifications']);      
+    Route::get('/tasks', [ApiController::class, 'list_tasks']);     
+    Route::get('/sitevisits', [ApiController::class, 'list_sitevisits']);        
+    Route::post('/sitevisits', [ApiController::class, 'create_sitevisit']);      
+
 });
