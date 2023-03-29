769e104e2257b2412a4cea80e87212ac02205d82
Afeez Aziz <afeezaziz@gmail.com>
2023-03-29 13:08:12 +0800

    Create ApiController.php
    9 functions for the API placeholder.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+        public function login(Request $request) {}
+        
+        public function list_projects(Request $request) {} 
+  
+        public function get_project(Request $request) {}   
+  
+        public function search_project(Request $request) {}     
+        
+        public function update_report(Request $request) {}   
+  
+        public function list_notifications(Request $request) {}   
+  
+        public function list_tasks(Request $request) {}   
+  
+        public function get_sitevisits(Request $request) {}   
+        
+        public function create_sitevisit(Request $request) {}   
+  
+}
